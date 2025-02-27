74d136d29ca66e6268b81c3a9da087ed45a1a32a
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-02-27 18:24:03 +0800

    添加目录遍历 Demo

--- src-manager/widget_register.ts
+++ src-manager/widget_register.ts
@@ -1,12 +1,10 @@
-const component = await electron.getAllWidgets()
+import { readDir, BaseDirectory } from '@tauri-apps/plugin-fs'
 
 
-let widgetList = component.widgets
-for (const widget of widgetList) {
-  widget.content = () => import(widget.relpath)
-}
+// 1、Resource 是 tauri.exe 所在目录，开发时在 src-tauri/target/debug
+// 2、权限问题
+  // - 检查拼写
+  // - 检查路径，注意 $RESOURCE != $RESOURCE/*
+const entrys = await readDir('', { baseDir: BaseDirectory.Resource })
 
-
-export const categorys = component.categorys
-export const widgets = widgetList
-export const number = component.number
+console.log(entrys)
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_fs::init())  // 注册 fs 插件，记得给 Cargo.toml 一并添加依赖
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
