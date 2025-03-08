c87b52b6b549804b4871cebc2ebd3bbd33dc34c4
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-03-09 03:24:16 +0800

    组件：请求封装改为 Tauri API

--- src-components/request.js
+++ src-components/request.js
@@ -1,10 +1,28 @@
+import { readFile, BaseDirectory } from '@tauri-apps/plugin-fs'
 import axios from 'axios'
 
-// const server = await window.electron.server()  // 临时禁用
+// 读取配置
+const data = await readFile(`./config/deskset.json`, { baseDir: BaseDirectory.Resource })
+const config = JSON.parse(new TextDecoder().decode(data))
 
+// 获取 port
+const port = config['server-port']
+
+// 获取 token
+const formData = new FormData()
+formData.append('username', config.username)
+formData.append('password', config.password)
+
+const repLogin = await axios.post(`http://127.0.0.1:${port}/v0/access/login`, formData, {
+  headers: { 'Content-Type': 'multipart/form-data' }
+})
+const token = repLogin.data.access_token
+
+
+// 导出
 export const desksetReq = axios.create({
-  baseURL: `http://127.0.0.1:6527`,
+  baseURL: `http://127.0.0.1:${port}`,
   headers: {
-    Authorization: `Bearer`
+    Authorization: `Bearer ${token}`
   }
 })
