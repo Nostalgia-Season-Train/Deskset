605bc06af546045f44884e43004547e67aa55353
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-03-10 00:41:34 +0800

    桌面：优化主题创建 + 读取主题样式

--- src-desktop/tauri.ts
+++ src-desktop/tauri.ts
@@ -8,20 +8,28 @@ import { mkdir, writeFile, BaseDirectory } from '@tauri-apps/plugin-fs'
 export const saveThemeDir = async (name: string, data: any) => {
   const encoder = new TextEncoder()
   const encodeData = encoder.encode(JSON.stringify(data))
-  await mkdir(`./themes/${ name }`, { baseDir: BaseDirectory.Resource })
+  await mkdir(`./themes/${ name }`, { baseDir: BaseDirectory.Resource, recursive: true })
   await writeFile(`./themes/${ name }/data.json`, encodeData, { baseDir: BaseDirectory.Resource })
 }
 
 // 读取主题
 import { readFile } from '@tauri-apps/plugin-fs'
+import { resourceDir, join } from '@tauri-apps/api/path'
+import { convertFileSrc } from '@tauri-apps/api/core'
 
 export const readThemeDir = async (themeName: string): Promise<Object> => { 
-  let theme = new Object() as { data: string }
+  let theme = new Object() as { data: string, style: string | undefined }
 
   try {
     const data = await readFile(`./themes/${ themeName }/data.json`, { baseDir: BaseDirectory.Resource })
     theme.data = JSON.parse(new TextDecoder().decode(data))
   } catch {}
+
+  try {
+    const style = await readFile(`./themes/${ themeName }/style.css`, { baseDir: BaseDirectory.Resource })
+    const stylePath = await join(await resourceDir(), 'themes', themeName, 'style.css')
+    theme.style = convertFileSrc(stylePath)
+  } catch (error) {}
 
   return theme
 }
