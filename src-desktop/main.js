de95d2eae0ecf55608144482aea4d04b9c657cba
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-03-11 15:52:02 +0800

    桌面：优化 Vite 打包体积

--- src-desktop/main.js
+++ src-desktop/main.js
@@ -1,11 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 
-import ElementPlus from 'element-plus'
-import zh_CN from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
-
-import 'echarts'
 
 import Desktop from './Desktop.vue'
 import drag from './widgetDrag'
@@ -15,6 +11,5 @@ import '../static/css/default-font.css'
 
 const app = createApp(Desktop)
 
-app.use(ElementPlus, { locale: zh_CN })
 app.use(drag)
 app.mount('#desktop')
