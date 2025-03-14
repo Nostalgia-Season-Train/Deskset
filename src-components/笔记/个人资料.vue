1621bf5f89a01dd9972a353337e31642e8b9bf26
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-03-14 17:03:29 +0800

    组件：新增组件 个人资料

---
+++ src-components/笔记/个人资料.vue
@@ -0,0 +1,75 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const profile = ref({
+  avatar: '',
+  name: '昵称',
+  bio: '签名'
+})
+// - avatar 头像
+// - name   昵称
+// - bio    签名
+// - [ ] 统计
+// - [ ] 链接（其他网站的资料）
+// - [ ] 成就
+
+
+// 配置
+const config = defineModel({ type: Object })
+
+if (Object.keys(config.value).length != 0) {
+  profile.value = config.value
+}
+</script>
+
+
+<template>
+<div class="profile">
+  <div class="left">
+    <div class="avatar">
+      <img :src="profile?.avatar" draggable="false"></img>
+    </div>
+  </div>
+  <div class="right">
+    <div class="name">{{ profile.name }}</div>
+    <div class="bio">{{ profile.bio }}</div>
+  </div>
+</div>
+</template>
+
+
+<style lang="less" scoped>
+.profile {
+  width: 280px;
+  background-color: #FFF7;
+
+  border: 1px solid white;
+  border-radius: 5px;
+
+  padding: 5px;
+
+  display: flex;
+  justify-content: space-between;
+  gap: 5px;
+
+  .avatar {
+    width: 100px;
+    height: 100px;
+    background-color: white;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .right {
+    flex: 1;
+    color: #FFF;
+    .name {
+      font-size: 20px;
+    }
+    .bio {
+      margin-top: 5px;
+    }
+  }
+}
+</style>
