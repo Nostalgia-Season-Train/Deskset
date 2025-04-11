fa66cf1b80e4e5c98d2076f0bdc6d080aa18c786
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-04-12 02:16:03 +0800

    组件：新增组件 翻页时钟、计时器

---
+++ src-components/简约系列/翻页时钟.vue
@@ -0,0 +1,166 @@
+<script lang="ts" setup>
+import { onMounted, onBeforeUnmount } from 'vue'
+import dayjs from 'dayjs'
+
+/* === 挂载后才能用 querySelector 匹配网页元素 === */
+onMounted(() => {
+  const hour_tens = document.querySelector('[data-hour-tens]')
+  const hour_ones = document.querySelector('[data-hour-ones]')
+  const min_tens = document.querySelector('[data-min-tens]')
+  const min_ones = document.querySelector('[data-min-ones]')
+  const sec_tens = document.querySelector('[data-sec-tens]')
+  const sec_ones = document.querySelector('[data-sec-ones]')
+  if (hour_tens == null) return
+  if (hour_ones == null) return
+  if (min_tens == null) return
+  if (min_ones == null) return
+  if (sec_tens == null) return
+  if (sec_ones == null) return
+
+  // DataVue：组件作用域 style scoped 创建的 data-v 属性
+  const getDataVueAttribute = (element: Element) => {
+    for (const attribute of element.getAttributeNames()) {
+      if (attribute.startsWith('data-v')) return attribute
+    }
+  }
+  const dataVue = getDataVueAttribute(sec_ones)
+  if (dataVue == null) return
+
+  const refresh = setInterval(async () => {
+    const time = dayjs().format('HHmmss')
+    flip(hour_tens, time[0], dataVue)
+    flip(hour_ones, time[1], dataVue)
+    flip(min_tens, time[2], dataVue)
+    flip(min_ones, time[3], dataVue)
+    flip(sec_tens, time[4], dataVue)
+    flip(sec_ones, time[5], dataVue)
+  }, 1000)
+  onBeforeUnmount(() => clearInterval(refresh))
+
+  const flip = async (flipCard: Element, newNum: string, dataVue: string) => {
+    const topHalf = flipCard.querySelector('.top')
+    const bottomHalf = flipCard.querySelector('.bottom')
+    if (topHalf == null || bottomHalf == null || topHalf.textContent == null) return
+
+    const oldNum = topHalf.textContent
+    if (newNum === oldNum) return
+
+    // setAttribute(dataVue, '') 添加对应 data-v 属性，让 style scoped 中的 CSS 生效
+    const topFlip = document.createElement('div')
+    topFlip.classList.add('top-flip')
+    topFlip.setAttribute(dataVue, '')
+    const bottomFlip = document.createElement('div')
+    bottomFlip.classList.add('bottom-flip')
+    bottomFlip.setAttribute(dataVue, '')
+
+    // 翻转时，topHalf、bottomFlip 视作新面，topFlip、bottomHalf 视作旧面
+    topFlip.textContent = oldNum
+    topFlip.addEventListener('animationstart', ev => topHalf.textContent = newNum)
+    topFlip.addEventListener('animationend', ev => topFlip.remove())
+    bottomFlip.textContent = newNum
+    bottomFlip.addEventListener('animationend', ev => {
+      bottomHalf.textContent = newNum
+      bottomFlip.remove()
+    })
+
+    // 插入动画元素 Flip，开始翻转
+    flipCard.append(topFlip, bottomFlip)
+  }
+})
+</script>
+
+
+<template>
+<div class="container">
+  <div class="segment">
+    <div class="flip-card" data-hour-tens>
+      <div class="top">0</div>
+      <div class="bottom">0</div>
+    </div>
+    <div class="flip-card" data-hour-ones>
+      <div class="top">0</div>
+      <div class="bottom">0</div>
+    </div>
+  </div>
+  <div class="segment">
+    <div class="flip-card" data-min-tens>
+      <div class="top">0</div>
+      <div class="bottom">0</div>
+    </div>
+    <div class="flip-card" data-min-ones>
+      <div class="top">0</div>
+      <div class="bottom">0</div>
+    </div>
+  </div>
+  <div class="segment">
+    <div class="flip-card" data-sec-tens>
+      <div class="top">0</div>
+      <div class="bottom">0</div>
+    </div>
+    <div class="flip-card" data-sec-ones>
+      <div class="top">0</div>
+      <div class="bottom">0</div>
+    </div>
+  </div>
+</div>
+</template>
+
+
+<style lang="less" scoped>
+.container {
+  display: flex;
+  gap: 50px;
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+}
+
+.segment {
+  .flip-card {
+    position: relative;
+    display: inline-flex;
+    flex-direction: column;
+
+    .top, .bottom, .top-flip, .bottom-flip {
+      height: .75em;
+      line-height: 1;
+      padding: .25em;
+      overflow: hidden;
+    }
+
+    .bottom, .bottom-flip {
+      display: flex;
+      align-items: flex-end;
+    }
+
+    div { color: #E0E0E0; font-size: 5vw; }
+    .top, .top-flip { background-color: #212121; }
+    .bottom, .bottom-flip { background-color: #757575; }
+  }
+}
+
+.top-flip {
+  position: absolute;
+  width: 100%;
+  animation: flip-top 250ms ease-in;
+  transform-origin: bottom;
+}
+@keyframes flip-top {
+  100% { transform: rotateX(90deg); }
+}
+
+.bottom-flip {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  animation: flip-bottom 250ms ease-in 250ms;
+  transform-origin: top;
+  transform: rotateX(90deg);
+}
+@keyframes flip-bottom {
+  100% { transform: rotateX(0deg); }
+}
+</style>
---
+++ src-components/简约系列/计时器.vue
@@ -0,0 +1,87 @@
+<script lang="ts" setup>
+import { Ref } from 'vue'
+
+class StopWatch {
+  _start: number | null = null
+  _isEnd: boolean = false  // step 判断结束的标志
+  _timeRef: Ref
+
+  constructor(timeRef: Ref) {
+    this._timeRef = timeRef
+  }
+
+  // step 传入 requestAnimationFrame 轮询回调自身
+  _step = (timestamp: number) => {
+    if (this._isEnd == true) {
+      this._start = null
+      this._isEnd = false
+      return
+    }
+
+    if (this._start == null) {
+      this._start = timestamp
+    }
+    const elapsed = timestamp - this._start
+    this._timeRef.value = this._msFormat(elapsed)
+
+    requestAnimationFrame(this._step)
+  }
+
+  // 毫秒 ms 转为 分:秒:十毫秒
+  _msFormat = (ms: number): string => {
+    const min = Math.floor(ms / 60000)           // 1 min = 60000 ms
+    const sec = Math.floor((ms % 60000) / 1000)  // 1 sec = 1000 ms
+    const tensms = Math.floor((ms % 1000) / 10)  // 10 ms
+
+    return [min, sec, tensms].map(n => String(n).padStart(2, '0')).join(':')
+  }
+
+  begin = () => {
+    if (this._start != null) {
+      return  // start != null 正在计时
+    }
+    requestAnimationFrame(this._step)
+  }
+
+  finish = () => {
+    this._isEnd = true
+  }
+}
+
+
+import { ref, onBeforeUnmount } from 'vue'
+
+const isTiming = ref(false)
+const time = ref('00:00:00')
+const stopwatch = new StopWatch(time)
+
+onBeforeUnmount(() => stopwatch.finish())  // 重要！step 不会自动停止
+</script>
+
+
+<template>
+<div class="container">
+  <div class="time">{{ time }}</div>
+  <div class="button">
+    <div v-if="isTiming == false" @click="stopwatch.begin(); isTiming = true">开始</div>
+    <div v-if="isTiming == true" @click="stopwatch.finish(); isTiming = false">结束</div>
+  </div>
+</div>
+</template>
+
+
+<style lang="less" scoped>
+.container {
+  width: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;  // time 变化会左右移动，后面优化
+  color: white;
+  .time {
+    font-size: 2em;
+  }
+  .button {
+    font-size: 1.2em;
+  }
+}
+</style>
