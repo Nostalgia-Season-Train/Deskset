dfd8471c7774570a18b7788f2905ca505f5a817b
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-04-12 23:48:02 +0800

    管理/主页（建设中）：增加侧边栏

--- src-manager/routers/HomePage.vue
+++ src-manager/routers/HomePage.vue
@@ -1,1 +1,21 @@
-<template>主页</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { ElButton, ElDrawer } from 'element-plus'
+import { SlidersHorizontal } from 'lucide-vue-next'
+
+import Sidebar from './HomePage/Sidebar.vue'
+
+const drawer = ref(false)
+</script>
+
+
+<template>
+  <!-- 内容区 -->
+  <div id="homepage-main" style="width: 100%; height: 100%; transform: translate(0, 0);">
+    <el-button @click="drawer = true"><SlidersHorizontal /></el-button>
+  </div>
+  <!-- 侧边栏 -->
+  <el-drawer v-model="drawer" direction="ltr" :with-header="false" append-to="#homepage-main">
+    <Sidebar />
+  </el-drawer>
+</template>
---
+++ src-manager/routers/HomePage/Sidebar.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+  <div class="dashboard">信息汇总</div>
+  <div>日程表</div>
+  <div>工作流</div>
+</div>
+</template>
