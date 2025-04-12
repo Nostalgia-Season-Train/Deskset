c17196ca1f24ff71ec5d72848a4f8553e6c5f878
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-04-13 00:59:45 +0800

    管理/主页：优化内容布局和按钮样式

--- src-manager/routers/HomePage.vue
+++ src-manager/routers/HomePage.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { ElButton, ElDrawer } from 'element-plus'
+import { ElButton, ElDrawer, ElScrollbar } from 'element-plus'
 import { SlidersHorizontal } from 'lucide-vue-next'
 
 import Sidebar from './HomePage/Sidebar.vue'
@@ -11,11 +11,18 @@ const drawer = ref(false)
 
 <template>
   <!-- 内容区 -->
-  <div id="homepage-main" style="width: 100%; height: 100%; transform: translate(0, 0);">
+  <el-scrollbar id="homepage-main" style="width: 100%; height: 100%; transform: translate(0, 0);">
     <el-button @click="drawer = true"><SlidersHorizontal /></el-button>
-  </div>
+  </el-scrollbar>
   <!-- 侧边栏 -->
   <el-drawer v-model="drawer" direction="ltr" :with-header="false" append-to="#homepage-main">
     <Sidebar />
   </el-drawer>
 </template>
+
+
+<style lang="less" scoped>
+::v-deep(.el-button) {
+  padding: 8px;
+}
+</style>
