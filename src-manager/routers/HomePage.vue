0a3a28c89f45ce0b0eb92d0dd5b04991ea7e6e11
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-05-14 16:55:54 +0800

    管理/主页 改进：流式响应 Obsidian 仓库上下线事件

--- src-manager/routers/HomePage.vue
+++ src-manager/routers/HomePage.vue
@@ -1,30 +1,49 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { ElButton, ElDrawer, ElScrollbar } from 'element-plus'
-import { SlidersHorizontal } from 'lucide-vue-next'
+import { inject, onUnmounted } from 'vue'
+import { AxiosStatic } from 'axios'
 
-import Sidebar from './HomePage/Sidebar.vue'
-import Dashboard from './HomePage/Dashboard.vue'
+const axios = inject('$axios') as AxiosStatic
+const controller = new AbortController()
 
-const drawer = ref(false)
+const refresh = async () => {
+  axios.get('/v0/note/obsidian-manager/noteapi/event', {
+    adapter: 'fetch',
+    responseType: 'stream',
+    signal: controller.signal
+  })
+  .then(response => {
+    const reader = response.data.getReader()
+    const decoder = new TextDecoder()
+
+    reader.read().then(function process({ done, value }) {
+      // 1、是否传输结束
+      if (done) {
+        return reader.read().then(refresh)
+      }
+
+      // 2、解析数据
+      const chunk = decoder.decode(value, { stream: true })
+      console.log(chunk)
+
+      // 3、回调自身
+      return reader.read().then(process)
+    })
+    .catch(error => console.log('fetch catch', error))
+  })
+  .catch(error => console.log('axios catch', error))
+}
+refresh()
+
+// controller.abort('caught by axios')
+onUnmounted(() => {
+  controller.abort('caught by fetch')
+})
 </script>
 
 
 <template>
-  <!-- 内容区 -->
-  <el-scrollbar id="homepage-main" style="width: 100%; height: 100%; transform: translate(0, 0);">
-    <el-button @click="drawer = true"><SlidersHorizontal /></el-button>
-    <Dashboard />
-  </el-scrollbar>
-  <!-- 侧边栏 -->
-  <el-drawer v-model="drawer" direction="ltr" :with-header="false" append-to="#homepage-main">
-    <Sidebar />
-  </el-drawer>
 </template>
 
 
 <style lang="less" scoped>
-::v-deep(.el-button) {
-  padding: 8px;
-}
 </style>
