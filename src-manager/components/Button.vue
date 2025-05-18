5c3bc7eccb93c96a628082776afc7452683cecd0
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-05-18 17:58:55 +0800

    管理 新增： - 通用按钮组件 - 通用进度条组件
    Vite 项目 改进：调整 tsconfig.json 配置，让 vscode 找到路径别名

---
+++ src-manager/components/Button.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="button-container">
+  <div class="button">
+    <div class="text">按钮</div>
+  </div>
+</div>
+</template>
+
+
+<style lang="less" scoped>
+.button-container {
+  --color: #FFF;
+
+  .button {
+    box-sizing: border-box;
+    width: 80px;
+    height: 30px;
+    border: solid 1px rgba(from var(--color) r g b / .1);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    color: #FFF;
+    font-size: 16px;
+
+    cursor: pointer;
+
+    transition: .3s ease;
+    &:hover {
+      box-shadow: 0 0 5px 0 rgba(from var(--color) r g b / .25);
+    }
+    &:active {
+      border-color: rgba(from var(--color) r g b / .5);
+    }
+  }
+}
+</style>
---
+++ src-manager/components/Progress.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const percent = ref<number>(50)
+</script>
+
+
+<template>
+<div class="progress-container">
+  <div class="progress">
+    <span class="percent" :style="`width: ${percent}%;`">{{ percent }}</span>
+  </div>
+</div>
+</template>
+
+
+<style lang="less" scoped>
+.progress-container {
+  .progress {
+    box-sizing: border-box;
+    width: 200px;
+    height: 12px;
+
+    display: flex;
+    justify-content: left;
+    align-items: center;
+
+    background: #333;
+    border: 1px solid #FFF;
+
+    .percent {
+      width: 0;
+      height: 100%;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      font-size: 14px;
+      color: #000;
+      background: #FFF;
+    }
+  }
+}
+</style>
