fb1252962779cd38be90b61d2118dbbd16b9b57b
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-06-17 00:09:10 +0800

    管理 新增：通用消息输入框组件

--- src-manager/components/Message.ts
+++ src-manager/components/Message.ts
@@ -38,3 +38,42 @@ export function message(
     document.body.appendChild(container)
   })
 }
+
+import MessageInputSFC from './MessageInputSFC.vue'
+
+export function messageInput(
+  title: string,
+  content: string,
+  placeholder: string,
+  cancel: string = '取消',
+  confirm: string = '确认'
+): Promise<string | null> {
+  return new Promise((resolve, reject) => {
+    const vnode = h(MessageInputSFC, {
+      title,
+      content,
+      placeholder,
+      cancel,
+      confirm,
+
+      onDestory: () => {
+        render(null, container)
+        document.body.removeChild(container)
+      },
+
+      onClose: () => {
+        resolve(null)
+      },
+      onCancel: () => {
+        resolve(null)
+      },
+      onConfirm: (input: string) => {
+        resolve(input)
+      }
+    })
+
+    const container = document.createElement('div')
+    render(vnode, container)
+    document.body.appendChild(container)
+  })
+}
