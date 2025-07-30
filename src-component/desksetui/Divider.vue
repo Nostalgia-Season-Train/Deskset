cce53c96d7ba2b9b25f102eabea60aa27990d367
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-07-31 01:16:10 +0800

    组件 新增：通用分割线组件 Divider.vue

---
+++ src-component/desksetui/Divider.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="w-full flex justify-center items-center">
+
+  <div class="w-1/10"><!-- 左：10% -->
+    <div class="w-full h-px bg-white"></div>
+  </div>
+
+  <div class="p-[5px] text-white">
+    <slot></slot>
+  </div>
+
+  <div class="flex-1"><!-- 右：填满 -->
+    <div class="w-full h-px bg-white"></div>
+  </div>
+
+</div>
+</template>
+
+
+<style scoped>
+/* 样式采用 Tailwind CSS 编写 */
+</style>
