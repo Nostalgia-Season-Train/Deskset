574c075555baaef1084f52924087a6d470a2fd16
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-08-01 01:11:55 +0800

    管理/设置/通用选项 改进：补完开关和下拉菜单

--- src-manager/routers/Setting/Common.vue
+++ src-manager/routers/Setting/Common.vue
@@ -1,23 +1,52 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const isAutostart = ref(false)
+const closeBehavior = ref('隐藏')
+
+/* === 子组件 === */
+import Switch from '#desksetui/Switch.vue'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger
+} from '#shadcn/components/ui/dropdown-menu'
+import Button from '#desksetui/Button.vue'
+</script>
+
+
 <template>
 <div>
 
   <div class="option">
     <div class="left">
       <div class="name">开机启动</div>
-      <div class="description">开机自动运行数字桌搭</div>
+      <div class="description">是否开机自动运行</div>
     </div>
     <div class="right">
-      <!-- 后面补上下拉菜单 -->
+      <Switch v-model="isAutostart"/>
     </div>
   </div>
 
   <div class="option">
     <div class="left">
       <div class="name">关闭行为</div>
-      <div class="description">点击关闭按钮时，是否隐藏到托盘</div>
+      <div class="description">点击关闭按钮时，是隐藏窗口还是直接退出</div>
     </div>
     <div class="right">
-      <!-- 后面补上下拉菜单 -->
+      <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+          <Button>{{ closeBehavior }}</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent class="w-56">
+          <DropdownMenuRadioGroup v-model="closeBehavior">
+            <DropdownMenuRadioItem value="隐藏">最小化到系统托盘</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="退出">退出应用</DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   </div>
 
