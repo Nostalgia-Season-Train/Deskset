04fe293489ce8bfe03b9dda75ba22f97e56d6f7f
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-08-10 16:00:00 +0800

    组件 改进：desksetui/divider 改为亮色模式

--- src-component/desksetui/Divider.vue
+++ src-component/desksetui/Divider.vue
@@ -2,15 +2,15 @@
 <div class="w-full flex justify-center items-center">
 
   <div class="w-1/10"><!-- 左：10% -->
-    <div class="w-full h-[0.5px]" style="background: #FFF7;"></div>
+    <div class="w-full h-[0.5px]" style="background: #3337;"></div>
   </div>
 
-  <div class="p-[5px]" style="color: #FFFC">
+  <div class="p-[5px]" style="color: #333C">
     <slot></slot>
   </div>
 
   <div class="flex-1"><!-- 右：填满 -->
-    <div class="w-full h-[0.5px]" style="background: #FFF7;"></div>
+    <div class="w-full h-[0.5px]" style="background: #3337;"></div>
   </div>
 
 </div>
