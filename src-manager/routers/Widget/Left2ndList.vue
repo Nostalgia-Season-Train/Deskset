237edc1187cb931e5896d20bf508610586c88061
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-08-12 22:46:24 +0800

    管理/部件 改进：左侧列表改为亮色模式

--- src-manager/routers/Widget/Left2ndList.vue
+++ src-manager/routers/Widget/Left2ndList.vue
@@ -8,7 +8,7 @@ import ElScrollbar from '#element-plus/ElScrollbar.vue'
 
 
 <template>
-<div class="left2ndlist">
+<div class="left2ndlist rounded-md">
   <ElScrollbar>
     <div
       v-for="widget in Array.from(activeWidgetMap.values())"
@@ -26,16 +26,16 @@ import ElScrollbar from '#element-plus/ElScrollbar.vue'
 .left2ndlist {
   width: 100%;
   height: 100%;
-  border: solid 1px rgba(#FFF, .3);
+  border: solid 1px rgba(#000, .3);
 
   // ElScrollbar 高度自适应
     // 注：left2ndlist 高度由父组件 flex 布局分配
   overflow: hidden;
 
   .item {
-    color: #FFF;
+    color: #000;
     &:hover {
-      background-color: #FFF3;
+      background-color: #0003;
     }
   }
 }
