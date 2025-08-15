54ade4e8324d269440a967d374a74572622fce87
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-08-16 00:52:48 +0800

    管理/欢迎 改进：优化简介描述

--- src-manager/routers/Welcome.vue
+++ src-manager/routers/Welcome.vue
@@ -20,13 +20,19 @@ const openReleasePage = () => {
     <div>{{ _t('欢迎使用数字桌搭 Deskset') }}</div>
     <div>一款桌面美化软件</div>
     <div>基于 Web 技术开发</div>
-    <div>实现桌面美化与笔记应用间的互动</div>
+    <div>定制专属于你的高效工作台</div>
   </div>
 
   <h2>介绍</h2>
-  <div>数字桌搭是一款专注整合笔记应用的桌面美化软件，旨在优化笔记的读写管理流程，从而提高个人生产力。</div>
+  <div>数字桌搭是一款开源的桌面美化应用，拥有多个美化部件。通过覆盖原始桌面，允许用户创建自己的个性化桌面，并能保存为主题后分享给其他人。</div>
+  <div>除此之外，数字桌搭还拥有强大的插件系统：
+    <ul>
+      <li>支持导入单个 vue 文件作为桌面部件</li>
+      <li>支持导入 py 模块以给部件提供数据接口</li>
+    </ul>
+  </div>
 
-  <h2>主要功能</h2>
+  <!-- <h2>主要功能</h2>
   <div>主页：以博客形式可视化笔记仓库</div>
   <div class="br"></div>
 
@@ -39,11 +45,11 @@ const openReleasePage = () => {
   </ul>
   <div class="br"></div>
 
-  <div>主题：轻松保存和应用主题，并通过 CSS 定制样式</div>
+  <div>主题：轻松保存和应用主题，并通过 CSS 定制样式</div> -->
 
-  <h2>发布 & 交流</h2>
+  <h2></h2>
   <div>项目地址：<a @click="openReleasePage()" @click.prevent href="#">数字桌搭 - Deskset</a></div>
-  <div>QQ 群：770889923</div>
+  <!-- <div>QQ 群：770889923</div> -->
 
 </div>
 </Scrollbar>
@@ -62,14 +68,15 @@ const openReleasePage = () => {
     align-items: center;
   }
   h2 {
-    margin: 1em 0 0 0;
+    margin: 0.5em 0 0 0;
     font-size: 1.25em;
   }
   .br {
     margin: 0.5em;
   }
   ul {
-    margin: 0 0 0 2em;
+    list-style-type: disc;
+    margin: 0 0 0 1.5em;
     padding: 0;
   }
   a {
