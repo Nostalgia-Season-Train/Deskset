aafcb0d07a7afc57dfbef77b428cd05743280086
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-01-17 21:43:00 +0800

    文档 新增：用户手册初始页和部件列表

--- 100-Docs/.vitepress/config.ts
+++ 100-Docs/.vitepress/config.ts
@@ -11,15 +11,36 @@ export default defineConfig({
       { text: '开发文档', link: '/develop' }
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+    sidebar: {
+      '/manual/': [
+        { text: '部件', collapsed: false, items: [
+          { text: '笔记', collapsed: true, items: [
+            { text: '数据库' },
+            { text: '数字统计' },
+            { text: '热力图' },
+            { text: '仓库状态' },
+            { text: '日记' }
+          ]},
+          { text: '时间和日期', collapsed: true, items: [
+            { text: '时钟' },
+            { text: '日历' },
+            { text: '秒表' },
+            { text: '定时器' },
+            { text: '日期计数器' },
+            { text: '闹钟' },
+            { text: '番茄钟' },
+            { text: '时间进度条' },
+            { text: '日期进度条' },
+            { text: '世界时钟' }
+          ]},
+          { text: '设备信息', collapsed: true, items: [
+            { text: '硬件监控' },
+            { text: '硬盘存储值' },
+            { text: '电池电量' }
+          ]}
+        ]}
+      ]
+    },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
