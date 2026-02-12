7359d563508a29e11c43161052139f8ff83f8285
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-02-12 15:04:56 +0800

    桌设前端>管理>部件 重构：添加部件的下拉菜单换用 ElDropdown 组件

--- 200-DesksetFront/src-manager/routers/Widget/Left1stMenu.vue
+++ 200-DesksetFront/src-manager/routers/Widget/Left1stMenu.vue
@@ -3,13 +3,12 @@ import { _t } from '#manager/main/i18n'
 
 /* === 子组件 === */
 import { Plus } from 'lucide-vue-next'
-import Button from '#shadcn/components/ui/button/Button.vue'
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger
-} from '#shadcn/components/ui/dropdown-menu'
+  ElButton,
+  ElDropdown,
+  ElDropdownMenu,
+  ElDropdownItem
+} from 'element-plus'
 
 
 /* === 选项 === */
@@ -31,16 +30,18 @@ const openMenu = async () => {
 <template>
 <div class="dropdown">
 
-  <DropdownMenu>
-    <DropdownMenuTrigger as-child>
-      <Button class="w-full" @click="openMenu"><Plus/></Button>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent class="w-full">
-      <DropdownMenuItem v-for="option in options" @click="$emit('select', { name: option })">
-        {{ option.startsWith(prefixMark) ? _t(option.replace(prefixMark, '')) : option }}
-      </DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+  <ElDropdown placement="bottom" trigger="click" style="width: 100%; height: 100%;">
+    <ElButton @click="openMenu" style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;">
+      <Plus/>
+    </ElButton>
+    <template #dropdown>
+      <ElDropdownMenu>
+        <ElDropdownItem v-for="option in options" @click="$emit('select', { name: option })">
+          {{ option.startsWith(prefixMark) ? _t(option.replace(prefixMark, '')) : option }}
+        </ElDropdownItem>
+      </ElDropdownMenu>
+    </template>
+  </ElDropdown>
 
 </div>
 </template>
