a99013fc70881e7b8a8e4787d72d9aa56ebd5b97
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-02-15 12:15:00 +0800

    桌设后端>笔记 重构：目录结构

--- 300-DesksetBack/src/deskset/router/note/__init__.py
+++ 300-DesksetBack/src/deskset/router/note/__init__.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends
-
-from deskset.router._unify import check_token
+from deskset.router._unify import check_token, DesksetRepJSON
 
 router_note = APIRouter(
     prefix='/v0/note',
@@ -8,9 +7,35 @@ router_note = APIRouter(
 )
 
 
-# 注册子路由
-from .obsidian._manager import router_obsidian_manager
+# ==== ObsidianManager ====
+from ._manager import router_obsidian_manager
+
 router_note.include_router(router_obsidian_manager)
 
-from .obsidian import router_obsidian
+
+# ==== Obsidian ====
+router_obsidian = APIRouter(
+    prefix='/obsidian', tags=['Obsidian'],
+    default_response_class=DesksetRepJSON
+)
+
+# 通用
+from .common import router_common
+router_obsidian.include_router(router_common)
+# 个性资料
+from .profile import router_profile
+router_obsidian.include_router(router_profile)
+# 日记
+from .diary import router_diary
+router_obsidian.include_router(router_diary)
+# 数据统计
+from .stats import router_stats
+router_obsidian.include_router(router_stats)
+# 搜索
+from .search import router_search
+router_obsidian.include_router(router_search)
+# 问候语
+from .greet import router_greet
+router_obsidian.include_router(router_greet)
+
 router_note.include_router(router_obsidian)
