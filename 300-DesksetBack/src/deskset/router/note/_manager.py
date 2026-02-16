02fe9d99d714e1ea118c19440ef59cb942eb85e4
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-02-16 17:45:00 +0800

    桌设后端>笔记 重构：优化 API 注释和空行

--- 300-DesksetBack/src/deskset/router/note/_manager.py
+++ 300-DesksetBack/src/deskset/router/note/_manager.py
@@ -38,28 +38,31 @@ class API:
         self._rpc = None
         self._event_active_leaf_change = Event()
 
-    # --- 状态 ---
+
+    # ==== 状态 Status ====
     @property
     def is_offline(self) -> bool:
         return self._rpc is None
 
-    # --- 事件 ---
+
+    # ==== 事件 Event ====
     # 例：接收 active-leaf-change 触发 self._event_active_leaf_change 事件
-    async def trigger_event(self, response: dict) -> None:
+    async def _trigger_event(self, response: dict) -> None:
         name = response.get('event', None)
+        # 确认 response.event 存在
         if not isinstance(name, str):
             await asyncify(logging.error)(f'Receive Obsidian event: non-string name {name!r}')
             return
-
-        self_name = f'_event_{name.replace('-', '_')}'
+        self_name = f'_event_{name.replace('-', '_')}'  # 转换事件名，例如 xxx-xx-xxxx => _event_xxx_xx_xxxx
         self_event = self.__dict__.get(self_name)
+        # 确认 要触发的事件 存在
         if not isinstance(self_event, Event):
             await asyncify(logging.error)(f'Receive Obsidian event: self.{self_name} not a Event {self_event!r}')
             return
         self_event.set()
         self_event.clear()
 
-    async def trigger_all_event(self) -> None:
+    async def _trigger_all_event(self) -> None:
         for attr_key, attr_value in list(self.__dict__.items()):
             if not attr_key.startswith('_event_'):
                 continue
@@ -73,14 +76,16 @@ class API:
             return  # 没有上线，不等待
         return await self._event_active_leaf_change.wait()
 
-    # --- RPC ---
+
+    # ==== 远程调用 RPC ====
+
+    # --- 仓库 ---
     class VaultStatus(TypedDict):
         note_num: int    # 笔记总数
         attach_num: int  # 附件总数
         useday_num: int  # 使用天数
         tag_num: int     # 标签总数
         task_num: int    # 任务总数
-
     async def get_vault_status(self) -> API.VaultStatus:
         if self._rpc is None:
             raise DesksetError(message='Obsidian not online')
@@ -89,7 +94,6 @@ class API:
     class Heat(TypedDict):
         date: str
         number: int
-
     async def get_heatmap(self, weeknum: int) -> list[API.Heat]:
         if self._rpc is None:
             raise DesksetError(message='Obsidian not online')
@@ -100,18 +104,17 @@ class API:
             raise DesksetError(message='Obsidian not online')
         return await self._rpc.call_remote_procedure('get_active_file', [])
 
+    # --- 查询建议 ---
     class SuggestFile(TypedDict):
         name: str  # 文件主名
         type: str  # 文件扩展名
         path: str  # 文件相对仓库的路径
-
     async def suggest_by_switcher(self, query: str) -> list[API.SuggestFile]:
         if self._rpc is None:
             raise DesksetError(message='Obsidian not online')
         return await self._rpc.call_remote_procedure('suggest_by_switcher', [query])
 
-
-    # ==== 日记 ====
+    # --- 日记 ---
     async def read_diary(self, dayid: str):
         if self._rpc is None:
             raise DesksetError(message='Obsidian not online')
@@ -122,8 +125,7 @@ class API:
             raise DesksetError(message='Obsidian not online')
         return await self._rpc.call_remote_procedure('list_diarys_in_a_month', [monthid])
 
-
-    # ==== Obsidian 窗口 ====
+    # --- Obsidian 窗口 ---
     async def open_vault(self):
         if self._rpc is None:
             raise DesksetError(message='Obsidian not online')
@@ -134,8 +136,7 @@ class API:
             raise DesksetError(message='Obsidian not online')
         return await self._rpc.call_remote_procedure('open_in_obsidian', [path])
 
-
-    # ==== 数据分析 ====
+    # --- 数据分析 ---
     async def filter_frontmatter(self, filter_group: object):
         if self._rpc is None:
             raise DesksetError(message='Obsidian not online')
@@ -174,7 +175,7 @@ async def rpc(websocket: WebSocket):
         while True:
             response = await websocket.receive_json()
             if response.get('datetime'):  # 单向事件：Obsidian > Deskset
-                await api.trigger_event(response)
+                await api._trigger_event(response)
             if response.get('id'):        # RPC 调用：Deskset > Obsidian > Deskset
                 await api._rpc.on_receive(response)
     except WebSocketDisconnect:
@@ -185,7 +186,7 @@ async def rpc(websocket: WebSocket):
       # 代替 event_offline 下线事件
       # 这样就不需要 asyncio.wait 和 asyncio.create_task 同时监听两个事件
       # 只等 event_{name} 触发后，判断一次 is_offline 状态即可下线
-    await api.trigger_all_event()
+    await api._trigger_all_event()
 
 
 # ==== 登录 ====
