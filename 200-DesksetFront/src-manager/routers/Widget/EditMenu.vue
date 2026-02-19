b4267a82e0d865875fbb2d0fde310ae0a0cd8e32
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-02-19 13:40:16 +0800

    桌设前端>管理>部件 改进：优化编译弹出框代码

--- 200-DesksetFront/src-manager/routers/Widget/EditMenu.vue
+++ 200-DesksetFront/src-manager/routers/Widget/EditMenu.vue
@@ -4,19 +4,12 @@ const widget = defineModel<
   RuntimeWidget & { option: NonNullable<RuntimeWidget['option']> }  // 传入前父组件已确保 option != undefined
 >({ required: true })
 
-import desktop from '#manager/global/page/desktop'
-const Edit = async (key: string, newValue: any) => {
-  await desktop.editWidget(widget.value.id, { [key]: newValue })  // 注意动态键用 [key] 而不用 key
-}
-
-import NoteFilter from './EditMenu/NoteFilter.vue'
-import NoteProp from './EditMenu/NoteProp.vue'
+// 注：没用 component + h 是因为其行为有时很诡异
+import EditMenuItem from './EditMenuItem.vue'
 import {
   ElScrollbar,
   ElTabs,
-  ElTabPane,
-  ElInput,
-  ElColorPicker
+  ElTabPane
 } from 'element-plus'
 </script>
 
@@ -25,43 +18,11 @@ import {
 <div class="edit-menu">
 <ElScrollbar max-height="80vh">
 
-  <!-- component 不方便调整样式 -->
-  <!-- 拆分 v-model，通过父组件自然改变 widget.model -->
-  <div class="items" v-for="item in widget.option.items">
-    <!-- *** 颜色选择器 *** -->
-    <div class="item" v-if="item.input == 'rgba'">
-      <div>{{ item.name }}</div>
-      <ElColorPicker show-alpha value-on-clear="#FFFF" :model-value="widget.model[item.key]" @update:model-value="Edit(item.key, $event)"/>
-    </div>
-  </div>
+  <EditMenuItem v-model="widget" :items="widget.option.items"/>
 
   <ElTabs class="tab" v-if="widget.option.tabs.length != 0" :default-value="widget.option.tabs[0].id">
     <ElTabPane v-for="tab in widget.option.tabs" :name="tab.id" :label="tab.text">
-      <div class="items" v-for="item in tab.items">
-        <!-- *** 文本输入框 *** -->
-        <div class="item" v-if="item.input == 'str'">
-          <div>{{ item.name }}</div>
-          <ElInput v-model="widget.model[item.key]" @change="Edit(item.key, widget.model[item.key])"/>
-        </div>
-        <!-- *** 数字输入框 *** -->
-        <div class="item" v-if="item.input == 'num'">
-          <div>{{ item.name }}</div>
-          <ElInput v-model="widget.model[item.key]" @change="Edit(item.key, Number(widget.model[item.key]))"/>
-        </div>
-        <!-- *** 颜色选择器 *** -->
-        <div class="item" v-if="item.input == 'rgba'">
-          <div>{{ item.name }}</div>
-          <ElColorPicker show-alpha value-on-clear="#FFFF" :model-value="widget.model[item.key]" @update:model-value="Edit(item.key, $event)"/>
-        </div>
-        <!-- *** 笔记过滤 *** -->
-        <div class="item" v-if="item.input == 'noteFilter'">
-          <NoteFilter class="note-filter" v-model="widget.model[item.key]" @change="Edit(item.key, widget.model[item.key])"/>
-        </div>
-        <!-- *** 笔记属性 *** -->
-        <div class="item" v-if="item.input == 'noteProp'">
-          <NoteProp class="note-prop" v-model="widget.model[item.key]" @change="Edit(item.key, widget.model[item.key])"/>
-        </div>
-      </div>
+      <EditMenuItem v-model="widget" :items="tab.items"/>
     </ElTabPane>
   </ElTabs>
 
@@ -74,23 +35,9 @@ import {
 .edit-menu {
   width: 100%;
 
-  .item {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    &>.el-input {
-      max-width: 50%;
-    }
-  }
   .tab:has(.note-filter) {
     width: 70vw;
     min-height: 80vh;
-  }
-  .note-filter {
-    width: 100%;
-  }
-  .note-prop {
-    width: 100%;
   }
 }
 </style>
---
+++ 200-DesksetFront/src-manager/routers/Widget/EditMenuItem.vue
@@ -0,0 +1,91 @@
+<script lang="ts" setup>
+import { RuntimeWidget, RegisterModelOptionItem } from '#manager/global/widget'
+const widget = defineModel<
+  RuntimeWidget & { option: NonNullable<RuntimeWidget['option']> }  // 传入前父组件已确保 option != undefined
+>({ required: true })
+const prop = defineProps<{ items: RegisterModelOptionItem[] }>()
+
+import desktop from '#manager/global/page/desktop'
+const Edit = async (key: string, newValue: any) => {
+  await desktop.editWidget(widget.value.id, { [key]: newValue })  // 注意动态键用 [key] 而不用 key
+}
+
+import {
+  ElInput,
+  ElColorPicker
+} from 'element-plus'
+import NoteFilter from './EditMenu/NoteFilter.vue'
+import NoteProp from './EditMenu/NoteProp.vue'
+</script>
+
+
+<template>
+<div class="items" v-for="item in prop.items">
+
+  <!-- *** 文本输入框 *** -->
+  <div class="item" v-if="item.input == 'str'">
+    <div>{{ item.name }}</div>
+    <ElInput
+      v-model="widget.model[item.key]"
+      @change="Edit(item.key, widget.model[item.key])"
+    />
+  </div>
+  <!-- *** 数字输入框 *** -->
+  <div class="item" v-if="item.input == 'num'">
+    <div>{{ item.name }}</div>
+    <ElInput
+      v-model="widget.model[item.key]"
+      @change="Edit(item.key, Number(widget.model[item.key]))"
+    />
+  </div>
+
+  <!-- *** 颜色选择器 *** -->
+  <div class="item" v-if="item.input == 'rgba'">
+    <div>{{ item.name }}</div>
+    <ElColorPicker
+      show-alpha
+      color-format="hex"
+      :clearable="false"
+      :model-value="widget.model[item.key]"
+      @update:model-value="Edit(item.key, $event)"
+    />
+  </div>
+
+  <!-- *** 笔记过滤 *** -->
+  <div class="item" v-if="item.input == 'noteFilter'">
+    <NoteFilter
+      class="note-filter"
+      v-model="widget.model[item.key]"
+      @change="Edit(item.key, widget.model[item.key])"
+    />
+  </div>
+  <!-- *** 笔记属性 *** -->
+  <div class="item" v-if="item.input == 'noteProp'">
+    <NoteProp
+      class="note-prop"
+      v-model="widget.model[item.key]"
+      @change="Edit(item.key, widget.model[item.key])"
+    />
+  </div>
+
+</div>
+</template>
+
+
+<style lang="less" scoped>
+.item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .el-input {
+    max-width: 50%;
+  }
+  .note-filter {
+    width: 100%;
+  }
+  .note-prop {
+    width: 100%;
+  }
+}
+</style>
