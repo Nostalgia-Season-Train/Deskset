6d68a2c54bdc69b0a34408f4e712bd29c022e66c
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-02-21 17:09:21 +0800

    构建 修复：从 release 复制 Tauri 程序

--- 400-Build/build.bat
+++ 400-Build/build.bat
@@ -10,6 +10,6 @@ call uv sync
 call python setup.py
 cd ..\
 
-copy 200-DesksetFront\src-tauri\target\debug\Deskset.exe 000-Dist
+copy 200-DesksetFront\src-tauri\target\release\Deskset.exe 000-Dist
 xcopy 300-DesksetBack\dist\DesksetBack 000-Dist /s
 tar -czf Deskset.zip 000-Dist
