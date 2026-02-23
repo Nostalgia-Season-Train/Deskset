6279d0fd19906603dc6fe0f5645019d5f3751617
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-02-24 01:39:59 +0800

    桌设前端>部件>数字统计 重构：重写样式，加入 Fluent Emoji Flat 图标

--- 200-DesksetFront/src-widget/note/num-stats.vue
+++ 200-DesksetFront/src-widget/note/num-stats.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
 import { ref, watch, toRaw } from 'vue'
 import axios from 'axios'
+import { getIconData } from '@iconify/utils'
+import { icons } from '@iconify-json/fluent-emoji-flat'
+
+const iconifyData = ref(getIconData(icons, 'page-facing-up'))
 
 const model = defineModel<{
   /* --- 基础 --- */
@@ -26,44 +30,68 @@ const refresh = async () => {
 }
 refresh()
 
-const openInObsidian = async () => {
-  await axios.post('/v0/note/obsidian/stats/filter-and-random-open-in-obsidian', toRaw(model.value.filterGroup))
-}
-
 watch(model.value, async () => await refresh())
 </script>
 
 
 <template>
-<div
-  class="note-stats"
-  :style="`width: ${model.width}px; height: ${model.height}px; background: ${model.backgroundColor};`"
-  @dblclick="openInObsidian"
->
-  <div class="title" :style="`color: ${model.titleColor};`">{{ model.title }}</div>
-  <div class="number" :style="`color: ${model.numberColor};`">{{ number }}</div>
+<div class="note-stats">
+
+  <div class="first">
+    <!-- width、height、viewBox 保持 32 不要动，否则图标会偏移，用 CSS 调整大小 -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" v-html="iconifyData?.body"></svg>
+    <span>{{ model.title }}</span>
+  </div>
+
+  <div class="second">
+    {{ number }}
+  </div>
+
 </div>
 </template>
 
 
 <style lang="less" scoped>
+* {
+  font-family: 'MiSansVF';
+}
+
 .note-stats {
   box-sizing: border-box;
+  width: 140px;
+  height: fit-content;
+  padding: 10px;
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  background: #FFFD;
+  border: solid 1px #FFF;  // 让子元素 margin 撑开 fit-content 高度而非溢出
+  border-radius: 5px;
+  box-shadow: 0 2px 4px #000A;
 
-  white-space: nowrap;
-  overflow: hidden;
-
-  .title {
-    font-size: 20px;
+  .first {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    svg {
+      padding: 4px;
+      width: 20px;
+      height: 20px;
+      background: #FFF;
+      border-radius: 5px;
+    }
+    span {
+      position: relative;
+      top: -1px;  // 视觉居中
+      color: #333C;
+      font-size: 18px;
+      font-weight: 350;
+    }
   }
-  .number {
-    font-size: 36px;
-    font-weight: 1000;
+  .second {
+    margin: 5px 0px 0px 3px;
+    color: #111;
+    font-size: 20px;
+    font-weight: 500;
+    font-feature-settings: 'tnum';
   }
 }
 </style>
