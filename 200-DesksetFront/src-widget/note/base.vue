97ea37af4da06402c8299497142ba0d749696aad
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-02-23 14:52:39 +0800

    桌设前端>部件>数据库 新增：完成排序功能

--- 200-DesksetFront/src-widget/note/base.vue
+++ 200-DesksetFront/src-widget/note/base.vue
@@ -15,7 +15,8 @@ const model = defineModel<{
   width: number,
   height: number,
   noteFilter: any,
-  noteProp: any
+  noteProp: any,
+  noteSort: any
 }>({ required: true })
 
 // 列宽：ElTableV2.columns[n].width
@@ -54,7 +55,59 @@ const refresh = async () => {
 
   const filterGroup = toRaw(model.value.noteFilter)
   const rep = await axios.post('/v0/note/obsidian/stats/filter-frontmatter', filterGroup)
-  data.value = rep.data.result
+  const result = rep.data.result
+  // Obsidian 数据库的隐式排序...
+  result.sort((a: any, b: any) => {
+    return a['file.fullname'].localeCompare(b['file.fullname'], 'zh-cn')
+  })
+
+  // 排序
+  const sorts = model.value.noteSort.sorts.toReversed()  // 优先排序在后，覆盖在前的次要排序
+  for (const sort of sorts) {
+    result.sort((a: any, b: any) => {
+      const aValue = a[sort.dataKey]
+      const bValue = b[sort.dataKey]
+      // 相同，顺序不变
+      if (aValue === bValue)
+        return 0
+      // 键不存在
+      if (aValue === undefined && bValue !== undefined)
+        return 1
+      if (aValue !== undefined && bValue === undefined)
+        return -1
+      // 值不存在
+      if (aValue === null && bValue !== null)
+        return 1
+      if (aValue !== null && bValue === null)
+        return -1
+      const compare = (m: any, n: any) => {
+        if (typeof m !== 'boolean' && typeof n === 'boolean')
+          return 1
+        if (typeof m === 'boolean' && typeof n !== 'boolean')
+          return -1
+        if (typeof m === 'boolean' && typeof n === 'boolean')
+          return m < n ? -1 : 1
+        // 比较数字
+        if (typeof m !== 'number' && typeof n === 'number')
+          return 1
+        if (typeof m === 'number' && typeof n !== 'number')
+          return -1
+        if (typeof m === 'number' && typeof n === 'number')
+          return m < n ? -1 : 1
+        // 比较字符串
+        if (typeof m === 'string' && typeof n === 'string')
+          return m.localeCompare(n, 'zh-cn')
+        // 鬼知道这是啥
+        return 0
+      }
+      if (sort.direct === 'asc')
+        return compare(aValue, bValue)
+      else
+        return -compare(aValue, bValue)
+    })
+  }
+
+  data.value = result
 }
 refresh()
 
