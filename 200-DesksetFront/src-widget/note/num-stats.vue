24d0c30ff98612e91259310dde4e2e2970a7a836
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-02-24 02:14:57 +0800

    桌设前端>部件>数字统计 改进：根据图标名称前缀，切换 Fluent 和 Noto 图标库

--- 200-DesksetFront/src-widget/note/num-stats.vue
+++ 200-DesksetFront/src-widget/note/num-stats.vue
@@ -2,7 +2,8 @@
 import { ref, watch, toRaw } from 'vue'
 import axios from 'axios'
 import { getIconData } from '@iconify/utils'
-import { icons } from '@iconify-json/fluent-emoji-flat'
+import { icons as fluentIcons } from '@iconify-json/fluent-emoji-flat'
+import { icons as notoIcons } from '@iconify-json/noto'
 
 const iconifyData = ref()
 
@@ -28,7 +29,11 @@ const refresh = async () => {
   const filterGroup = toRaw(model.value.filterGroup)
   const rep = await axios.post('/v0/note/obsidian/stats/filter-frontmatter', filterGroup)
   number.value = rep.data.result.length
-  iconifyData.value = getIconData(icons, model.value.icon)
+  const [set, name = ''] = model.value.icon.split(':')
+  if (set === 'fluent-emoji-flat')
+    iconifyData.value = getIconData(fluentIcons, name)
+  if (set === 'noto')
+    iconifyData.value = getIconData(notoIcons, name)
 }
 refresh()
 
@@ -41,7 +46,13 @@ watch(model.value, async () => await refresh())
 
   <div class="first">
     <!-- width、height、viewBox 保持 32 不要动，否则图标会偏移，用 CSS 调整大小 -->
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" v-html="iconifyData?.body"></svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="iconifyData?.width"
+      :height="iconifyData?.height"
+      :viewBox="`0 0 ${iconifyData?.width} ${iconifyData?.height}`"
+      v-html="iconifyData?.body"
+    ></svg>
     <span>{{ model.title }}</span>
   </div>
 
