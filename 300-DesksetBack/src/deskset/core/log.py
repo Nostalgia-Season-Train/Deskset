1f8e5d6b1292d65f77503c74ba4511df52fed024
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-03-04 14:19:24 +0800

    桌设后端>核心>日志 改进：生成带日期时间的日志文件

--- 300-DesksetBack/src/deskset/core/log.py
+++ 300-DesksetBack/src/deskset/core/log.py
@@ -1,18 +1,23 @@
 LOG_DIR  = 'logs'                   # 日志目录
-LOG_FILE = 'DesksetBackLatest.log'  # 日志文件
+# LOG_FILE = 'DesksetBackLatest.log'  # 日志文件
+
+
+from datetime import datetime
+
+log_file = f'DesksetBack{datetime.now().strftime('%Y%m%d%H%M%S')}.log'
 
 
 from pathlib import Path
 
 Path(f'./{LOG_DIR}').mkdir(exist_ok=True)             # 创建日志目录，存在则跳过
-Path(f'./{LOG_DIR}/{LOG_FILE}').touch(exist_ok=True)  # 创建日志文件，存在则跳过
-open(f'./{LOG_DIR}/{LOG_FILE}', 'w').close()          # 清空（上次）日志文件
+Path(f'./{LOG_DIR}/{log_file}').touch(exist_ok=True)  # 创建日志文件，存在则跳过
+# open(f'./{LOG_DIR}/{LOG_FILE}', 'w').close()          # 清空（上次）日志文件
 
 
 import logging
 
 logging.basicConfig(
-    filename=f'./{LOG_DIR}/{LOG_FILE}',
+    filename=f'./{LOG_DIR}/{log_file}',
     # filemode 注释
       # 注 1：目录不存在也会抛出 FileNotFoundError 异常
       # 注 2：用 a 模式写入，w 模式会意外覆盖运行时日志
