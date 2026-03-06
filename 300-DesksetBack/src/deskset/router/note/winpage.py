ea99be2face00f8bd3b4be25fd458d7bdf85bef2
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-03-06 22:19:00 +0800

    桌设后端>路由>笔记 改进：窗口页面端点命名

--- 300-DesksetBack/src/deskset/router/note/winpage.py
+++ 300-DesksetBack/src/deskset/router/note/winpage.py
@@ -3,9 +3,9 @@ from ._api import noteapi
 
 router_winpage = APIRouter(prefix='/winpage')
 
-@router_winpage.get('/open_vault_on_obsidian')
-async def open_vault_on_obsidian():
+@router_winpage.get('/open_vault')
+async def open_vault():
     return await noteapi.open_vault_on_obsidian()
-@router_winpage.post('/open_file_on_obsidian')
-async def open_file_on_obsidian(path: str = Body(...)):
+@router_winpage.post('/open_file')
+async def open_file(path: str = Body(...)):
     return await noteapi.open_file_on_obsidian(path)
