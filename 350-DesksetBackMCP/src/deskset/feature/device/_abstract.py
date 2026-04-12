0463ea6cd1a8661e00eb9f2ebdc8b274c6c21f0b
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-04-12 16:55:24 +0800

    桌设后端MCP>特性&路由>设备 改进：简化路由地址和响应体格式，并给 OpenAPI 添加类型

--- 350-DesksetBackMCP/src/deskset/feature/device/_abstract.py
+++ 350-DesksetBackMCP/src/deskset/feature/device/_abstract.py
@@ -1,19 +1,52 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
+from typing import TypedDict
 
 
 class AbstractDevice(ABC):
+    class Monitor(TypedDict):
+        class Cpu(TypedDict):
+            percent: float
+            freq: float
+            count: int
+        class Ram(TypedDict):
+            percent: float
+            used: float
+            total: int
+        class Disk(TypedDict):
+            percent: float
+        class Network(TypedDict):
+            sent: float
+            recv: float
+        cpu: AbstractDevice.Monitor.Cpu
+        ram: AbstractDevice.Monitor.Ram
+        disk: AbstractDevice.Monitor.Disk
+        network: AbstractDevice.Monitor.Network
     @abstractmethod
-    def monitor(self) -> dict:
+    def monitor(self) -> AbstractDevice.Monitor:
         pass
 
+    class Disk(TypedDict):
+        root: str
+        total: int | float
+        free: int | float
+        percent: float
     @abstractmethod
-    def disk(self, is_gb: bool = True) -> list[dict]:
+    def disk(self, is_gb: bool = True) -> AbstractDevice.Disk:
         pass
 
+    class Battery(TypedDict):
+        isplug: bool
+        percent: int
     @abstractmethod
-    def battery(self) -> dict:
+    def battery(self) -> AbstractDevice.Battery:
         pass
 
+    class System(TypedDict):
+        name: str
+        system: str
+        version: str
+        machine: str
     @abstractmethod
-    def system(self) -> dict:
+    def system(self) -> AbstractDevice.System:
         pass
--- 350-DesksetBackMCP/src/deskset/router/device.py
+++ 350-DesksetBackMCP/src/deskset/router/device.py
@@ -1,4 +1,5 @@
 # ==== Device ====
+from deskset.feature.device._abstract import AbstractDevice
 from deskset.feature.device import device
 
 
@@ -7,27 +8,26 @@ from fastapi import APIRouter, Depends
 from deskset.router._unify import check_token, DesksetRepJSON
 
 router_device = APIRouter(
-    prefix='/v0/device', tags=['device'],
+    prefix='/device', tags=['device'],
     dependencies=[Depends(check_token)],
-    default_response_class=DesksetRepJSON
 )
 
 # 硬件监控
-@router_device.get('/monitor')
+@router_device.get('/monitor', response_model=AbstractDevice.Monitor)
 def monitor():
     return device.monitor()
 
 # 硬盘存储值
-@router_device.get('/disk')
+@router_device.get('/disk', response_model=AbstractDevice.Disk)
 def disk():
     return device.disk()
 
 # 电池电量
-@router_device.get('/battery')
+@router_device.get('/battery', response_model=AbstractDevice.Battery)
 def battery():
     return device.battery()
 
 # 系统信息
-@router_device.get('/system')
+@router_device.get('/system', response_model=AbstractDevice.System)
 def system():
     return device.system()
