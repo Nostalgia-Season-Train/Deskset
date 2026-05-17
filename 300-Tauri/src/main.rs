5f4164aad80734d7874820a78678169bd8010e27
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-05-17 10:12:46 +0800

    Tauri 改进：优化运行逻辑

--- 300-Tauri/src/main.rs
+++ 300-Tauri/src/main.rs
@@ -1,6 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    deskset_lib::run()
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  /* ==== 创建应用 ==== */
+  let app = tauri::Builder::default()
+    .plugin(tauri_plugin_opener::init())
+    .build(tauri::generate_context!())
+    .expect("error while running tauri application");
+
+
+  /* ==== 启动应用 ==== */
+  app.run(|_app_handle, e| match e {
+    // 当所有窗口关闭后，阻止 Tauri 退出
+    tauri::RunEvent::ExitRequested { api, .. } => {
+      api.prevent_exit();
+    }
+    _ => {}
+  });
+
+
+  /* ==== 正常退出 ==== */
+  Ok(())
 }
