9b5eb9e01037e32e203b843d13ee06c101dd4f3f
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-05-29 01:45:00 +0800

    Vite>feature>widget 改进：细化部件类型

--- 200-Vite/src/feature/widget/index.ts
+++ 200-Vite/src/feature/widget/index.ts
@@ -1,5 +1,4 @@
 export type {
   Widgetcls,
-  StorageWidget,
-  RuntimeWidget
+  Widget
 } from './type'
--- 200-Vite/src/feature/widget/type.d.ts
+++ 200-Vite/src/feature/widget/type.d.ts
@@ -42,39 +42,49 @@ export interface Widgetcls extends WidgetclsID, WidgetclsRegv {
   // - 编译出的 Vue SFC 代码函数
   // - 使用方法：const vnode = h({ render: main }); render(vnode, el)
   main: Function
+  // - [ ] 待定：部件模型验证
+  modelValidate?: unknown
+  // - [ ] 待定：部件选项组件
+  optionComponent?: unknown
 }
 
 
 /* ==== 部件 Widget ==== */
-export interface StorageWidget {
+
+/* --- 部件标识 --- */
+export interface WidgetID extends WidgetclsID {
   id: string         // 部件（实例）唯一标识符
   path: string       // 同部件（类）路径
   beInline: boolean  // 同部件（类）内联标识
+}
 
+/* --- 部件 --- */
+export interface Widget extends WidgetID {
   // 部件标题
   // - 默认等于部件名称
   // - 附：只在管理页显示，但为了方便逻辑，跟其他属性一样在桌面页存储
   title: string
 
+  // 部件 CSS 类名
+  // - 作用：切换 CSS 类名以改变部件（容器）状态
   isDragLock: boolean         // 部件是否锁定拖动
   isDisableInteract: boolean  // 部件是否禁用交互（事件）
   isAutoHide: boolean         // 部件是否自动隐藏（鼠标移入时显示）
 
+  // 部件 CSS 属性
+  // - 作用：设置 CSS 属性以改变部件（容器）偏移/位置、缩放/大小、不透明度/透明度
   left: number     // 部件向右偏移
   top: number      // 部件向下偏移
   scale: number    // 部件缩放
   opacity: number  // 部件不透明度
 
-  // 部件模型
-  // - 哪怕部件类没有模型定义，也会创建一个空对象（Object.create(null)）用以占位
-  model: Record<string, any>
-}
-export interface RuntimeWidget extends StorageWidget {
   // 部件坐标
-  // - 注意区分 left、top，x、y 是部件中心到左上角的距离
+  // - 注 1：区分于 left、top，x、y 是部件中心到左上角的距离
+  // - 注 2：部件实际位置由 left、top 决定，x、y 仅用于展示
   x: number
   y: number
 
-  // 部件选项
-  option?: unknown
+  // 部件模型
+  // - 哪怕部件类没有模型定义，也会创建一个空对象（Object.create(null)）用以占位
+  model: Record<string, any>
 }
