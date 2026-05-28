1ffce07496926e036a3b93a73d86a0ccb49051a3
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-05-28 15:28:08 +0800

    Vite>feature>widget 改进：细化部件类类型

--- 200-Vite/src/feature/widget/type.d.ts
+++ 200-Vite/src/feature/widget/type.d.ts
@@ -1,24 +1,28 @@
 /* ==== 部件类 Widgetcls ==== */
 // - 备选名：部件模板、部件蓝图
-export interface Widgetcls {
+// - 作用：1个 部件类 就是 1个 Vue组件，可以构造 N个 部件
+
+/* --- 部件类标识 --- */
+// - 作用：标识是哪个部件类
+export interface WidgetclsID {
   // 部件路径
   path: string
   // 部件内联标识
   // - 内联部件是提前写好并打包进数字桌搭的部件
   // - 附：be 代表特殊布尔属性区分 is
   beInline: boolean
+}
 
-  // 部件主函数
-  // - 编译出的 Vue SFC 代码函数
-  // - 使用方法：const vnode = h({ render: main }); render(vnode, el)
-  main: Function
-
-  /* --- 部件（类）信息 Widget(cls)Info --- */
+/* --- 部件类注册值 --- */
+// - 作用：通过 json 文件定义的部件类元数据
+export interface WidgetclsInfo {
+  // 部件（类）信息
   name: string      // 部件名称
   author: string    // 部件作者
   version: string   // 部件版本
   descript: string  // 部件描述
-
+}
+export interface WidgetclsRegv extends WidgetclsInfo {
   // 部件模型定义
   // - 实例化为部件模型 model
   modelDef?: Record<string, {
@@ -30,6 +34,14 @@ export interface Widgetcls {
   optionDef?: {
     unknown: unknown  // - [ ] 待定
   }[]
+}
+
+/* --- 部件类 --- */
+export interface Widgetcls extends WidgetclsID, WidgetclsRegv {
+  // 部件主函数
+  // - 编译出的 Vue SFC 代码函数
+  // - 使用方法：const vnode = h({ render: main }); render(vnode, el)
+  main: Function
 }
 
 
